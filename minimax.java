0a4a3811524ce9230e3679d46d6ead4c2cf2eacc
sharique14398 <48406856+sharique14398@users.noreply.github.com>
2020-12-20 14:53:21 +0530

    Update minimax.java

--- minimax.java
+++ minimax.java
@@ -1,18 +1,5 @@
 package minimax;
 
-/*public class minimax {
-
-	public minimax() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-*/
 import java.util.*;
 
 class Point {
